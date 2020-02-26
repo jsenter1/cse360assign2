0146e2d5d75df224a6411448d0038d0be9b3aedc
Jerrel <Jerrel@DESKTOP-ND4BID7.3520.dhcp.asu.edu>
2020-02-26 09:02:02 -0700

    First commit

---
+++ assign1/src/assign1/SimpleList.java
@@ -0,0 +1,152 @@
+package assign1;
+
+/**
+ * 
+ * This class represents an array and its operations. The size of the array is
+ * set to 10. The operations include for an array are add, remove, search, count
+ * and toString.
+ * 
+ * @author Jerrel Senter Class ID: 181 Assignment Number: 1
+ */
+public class SimpleList {
+
+	private int[] list;
+	private int count;
+
+	/**
+	 * Default Constructor
+	 */
+	public SimpleList() {
+
+		list = new int[10];
+		count = 0;
+
+	}
+
+	/**
+	 * This method is used to add a number in the start of the array and the rest of
+	 * the array is pushed forward. If the array is full, the last element is
+	 * dropped from the list and new is added at the start. It also increments the
+	 * count when a new element is added. The maximum count will remain 10 as size
+	 * of array cannot exceeds 10.
+	 * 
+	 * @param number - The number which needs to be added in the array.
+	 */
+
+	public void add(int number) {
+
+		for (int integer = (count - 1); integer >= 0; integer--) {
+			if (integer == 9) {
+				list[integer] = list[integer];
+			} else {
+				list[integer + 1] = list[integer];
+			}
+		}
+
+		list[0] = number;
+
+		if (count < 10) {
+			count++;
+		}
+
+	}
+
+	/**
+	 * This method is used to remove a number from the array and the rest of the
+	 * array is pushed backward. It also decrement the count when the element is
+	 * removed (if found). The minimum count will remain 0 as size of array cannot
+	 * be less than 0. If passed number is not found. Nothing happens
+	 * 
+	 * @param number - The number which needs to be removed from the array.
+	 */
+
+	public void remove(int number) {
+
+		boolean numberFound = false;
+
+		for (int integer = 0; integer < count; integer++) {
+
+			if (!numberFound && list[integer] == number) {
+				numberFound = true;
+			}
+
+			if (numberFound && integer < count) {
+
+				if (integer == 9) {
+					list[integer] = list[integer];
+				} else {
+					list[integer] = list[integer + 1];
+				}
+
+			}
+		}
+
+		if (numberFound) {
+			count--;
+		}
+
+	}
+
+	/**
+	 * This method is used to return the count i.e. the current number of elements
+	 * in the array.
+	 * 
+	 * @return count - Number of elements in array.
+	 */
+
+	public int count() {
+
+		return count;
+
+	}
+
+	/**
+	 * This method is used to make a string representation of the array separated by
+	 * a space. The space does not come after the last element.
+	 * 
+	 * @param string representation of the array.
+	 */
+
+	public String toString() {
+
+		String arrayRepresentationAsString = "";
+
+		for (int integer = 0; integer < count; integer++) {
+
+			if (integer == count - 1) {
+				arrayRepresentationAsString += list[integer];
+			} else {
+				arrayRepresentationAsString += list[integer] + " ";
+			}
+
+		}
+
+		return arrayRepresentationAsString;
+
+	}
+
+	/**
+	 * This method is used to search the passed number in the array and return the
+	 * index of this number if found otherwise it will return -1.
+	 * 
+	 * @param number - which need to be searched.
+	 * @return index of number if found other -1.
+	 */
+
+	public int search(int number) {
+
+		int index = -1;
+
+		for (int integer = 0; integer < count; integer++) {
+
+			if (list[integer] == number) {
+				System.out.println(integer);
+				return integer;
+			}
+
+		}
+
+		return index;
+	}
+
+}
---
+++ assign1/src/assign1/SimpleListTest.java
@@ -0,0 +1,101 @@
+package assign1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 
+ * This class is a Test class and is used to test the SimpleList class. It tests
+ * all the methods implemented in the SimpleList class. It also checks the
+ * constructor of the class.
+ * 
+ * @author Jerrel Senter Class ID: 181 Assignment Number: 1
+ */
+public class SimpleListTest {
+
+	@Test
+	public void constructorTest() {
+		SimpleList simpleList = new SimpleList();
+		assertEquals(0, simpleList.count());
+	}
+
+	@Test
+	public void addTest() {
+		SimpleList simpleList = new SimpleList();
+		simpleList.add(10);
+		simpleList.add(20);
+		assertEquals(2, simpleList.count());
+		simpleList.add(30);
+		simpleList.add(40);
+		simpleList.add(50);
+		simpleList.add(60);
+		assertEquals(6, simpleList.count());
+		simpleList.add(70);
+		simpleList.add(80);
+		simpleList.add(90);
+		simpleList.add(100);
+		assertEquals(10, simpleList.count());
+		simpleList.add(110);
+		simpleList.add(120);
+		assertEquals(10, simpleList.count());
+	}
+
+	@Test
+	public void removeTest() {
+		SimpleList simpleList = new SimpleList();
+		simpleList.remove(3);
+		assertEquals(0, simpleList.count());
+		simpleList.add(5);
+		simpleList.add(10);
+		simpleList.add(15);
+		simpleList.remove(10);
+		assertEquals(2, simpleList.count());
+	}
+
+	@Test
+	public void countTest() {
+		SimpleList simpleList = new SimpleList();
+		assertEquals(0, simpleList.count());
+		simpleList.add(20);
+		simpleList.add(40);
+		simpleList.remove(20);
+		assertEquals(1, simpleList.count());
+	}
+
+	@Test
+	public void toStringTest() {
+		SimpleList simpleList = new SimpleList();
+		simpleList.add(10);
+		assertEquals("10", simpleList.toString());
+		simpleList.add(20);
+		simpleList.add(30);
+		simpleList.add(40);
+		simpleList.add(50);
+		simpleList.add(60);
+		assertEquals("60 50 40 30 20 10", simpleList.toString());
+		simpleList.add(70);
+		simpleList.add(80);
+		simpleList.add(90);
+		simpleList.add(100);
+		simpleList.add(110);
+		assertEquals("110 100 90 80 70 60 50 40 30 20", simpleList.toString());
+		simpleList.remove(80);
+		assertEquals("110 100 90 70 60 50 40 30 20", simpleList.toString());
+		simpleList.remove(110);
+		assertEquals("100 90 70 60 50 40 30 20", simpleList.toString());
+		simpleList.remove(20);
+		assertEquals("100 90 70 60 50 40 30", simpleList.toString());
+	}
+
+	@Test
+	public void searchTest() {
+		SimpleList simpleList = new SimpleList();
+		simpleList.add(20);
+		simpleList.add(40);
+		simpleList.add(50);
+		assertEquals(0, simpleList.search(50));
+		assertEquals(1, simpleList.search(40));
+		assertEquals(2, simpleList.search(20));
+	}
+}
